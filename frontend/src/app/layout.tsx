3ba9a4f3e25fb80bfaff0c7f257fae925fb0e695
Ioanna Damianov <damianov.io@northeastern.edu>
2025-09-25 19:32:54 -0400

    started clerk integration

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,6 +7,15 @@ import Footer from "../../components/footer";
 import "./globals.css";
 import { usePathname } from "next/navigation";
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 const leagueSpartan = League_Spartan({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
@@ -20,16 +29,22 @@ export default function RootLayout({
 }>) {
   const pathname = usePathname();
   return (
+    <ClerkProvider>
     <html lang="en" className={leagueSpartan.variable}>
       <Head>
         <title>Foresight</title>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </Head>
       <body className={`${leagueSpartan.className} flex flex-col min-h-screen`}>
-        {pathname !== "/" && <Navbar />}
+        <SignedOut>
+              <SignInButton />
+              <SignUpButton></SignUpButton>
+        </SignedOut>
+        <Navbar />
         <main className="flex-1 flex">{children}</main>
         <Footer />
       </body>
     </html>
+    </ClerkProvider>
   );
 }
