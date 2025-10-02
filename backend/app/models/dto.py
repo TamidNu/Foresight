59ea3f28e3a705ae3a72917ca92eae0317a8fcbe
Madhav <mdnair230@gmail.com>
2025-10-02 19:56:01 -0400

    readded phone alias

--- backend/app/models/dto.py
+++ backend/app/models/dto.py
@@ -1,5 +1,8 @@
 from typing import Annotated
 from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, EmailStr, StringConstraints, ConfigDict
+
+Phone = Annotated[str, StringConstraints(strip_whitespace=True, min_length=7, max_length=20)]
 
 # Request DTO: what the client sends when creating a user
 class UserCreateRequest(BaseModel):
@@ -8,7 +11,7 @@ class UserCreateRequest(BaseModel):
     username: str
     password: str
     email: str
-    phone_number: int
+    phone_number: Phone
 
 # Response DTO: what you send back to the client after creating a user
 class UserResponse(BaseModel):
