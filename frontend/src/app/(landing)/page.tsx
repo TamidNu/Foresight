c9f8a7eb821fd1185a4aef5ff3cb5c73217a36fb
anyakrishnamony13 <krishnamony.a@northeastern.edu>
2025-10-02 19:32:54 -0400

    restructuring frontend setup

--- frontend/src/app/(landing)/page.tsx
+++ frontend/src/app/(landing)/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Typewriter from "../components/typewriter";
+import Typewriter from "../../components/typewriter";
 import {
   SignedIn,
   SignedOut,
