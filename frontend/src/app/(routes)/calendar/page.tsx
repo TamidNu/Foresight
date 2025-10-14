7bc1d7f07412fa82b59644089c1274609c539e08
Ioanna Damianov <damianov.io@northeastern.edu>
2025-10-13 22:34:26 -0400

    added calendar files, following frontend architecture

---
+++ frontend/src/app/(routes)/calendar/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React from "react";
+
+export default function Calendar() {
+  const textColor = "text-[#013172]"; 
+  const borderColor = "border-[#D1D5DB]"; 
+  const fontClass = "font-sans";
+
+
+  return;
+}
+
---
+++ frontend/src/components/calendar.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+import { ICalendar } from "../types/calendar";
+import { IDay } from "../types/day";
+
+interface CalendarProps {
+  calendar: ICalendar;
+}
+
+export default function Calendar() {
+  const textColor = "text-[#013172]"; 
+  const borderColor = "border-[#D1D5DB]"; 
+  const fontClass = "font-sans";
+
+
+  return;
+}
+
